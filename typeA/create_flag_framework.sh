d52dd853e9fcc038e2ce42151b72deee29fa36d8
C.J. Bott <cjamesbott@gmail.com>
2023-09-01 11:28:16 -0500

    Automated things

---
+++ typeA/create_flag_framework.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Will create a directory containing subdirectories for each (partial) flag manifold in n-space.
+
+# First, user inputs the n for the particular n-space they care about.
+read -p "You want to create flags in n-space for what n? " n
+
+# Make a directory called Flags-In-n-Space where the subdirectories will live.
+mkdir Flags-In-$n-Space
+
+sed -i "1s/.*/N = $n/" createflags.m2
+M2 createflags.m2
+
+file1="allconflags.txt"
+file2="allbrackflags.txt"
+while IFS= read -r line1 && IFS= read -r line2 <&3; do
+  mkdir Flags-In-$n-Space/$line1
+  sed -i "26s/.*/FlagManifold:=$line2;/" GenerateProblems.maple
+  maple GenerateProblems.maple
+  rm -rf $line1
+  sed -i "172s/.*/inputfile = get \"$line1.txt\";/" frobenius.m2
+  sed -i "177s/.*/f = \"frobenius_output-$line1.txt\" << \"\";/" frobenius.m2
+  M2 frobenius.m2
+  mv $line1.txt Flags-In-$n-Space/$line1/
+  mv frobenius_output-$line1.txt Flags-In-$n-Space/$line1/
+
+done < "$file1" 3< "$file2"
+
+rm allconflags.txt
+rm allbrackflags.txt
---
+++ typeA/createflags.m2
@@ -0,0 +1,43 @@
+N = 5
+
+allFlags = method()
+allFlags(ZZ) := (n) -> (
+    allsubs = subsets(toList(1..(n-1)));
+    modsubs = delete({},allsubs);
+    flaglist = {};
+    for sub in modsubs do(
+    flaglist = append(flaglist,append(sub,n)));
+    return(flaglist))
+
+conflags = method()
+conflags(ZZ) := (n) -> (
+    flaglist = allFlags(n);
+    flags = {};
+    for flag in flaglist do(
+	conflag = {"F"};
+	for i in flag do(
+	    conflag = append(conflag,toString(i)));
+	flags = append(flags,concatenate(conflag)));
+    return(flags))
+
+brackflags = method()
+brackflags(ZZ) := (n) -> (
+    flaglist = allFlags(n);
+    flags = {};
+    for flag in flaglist do(
+        brackflag = [];
+        for i in flag do(
+            brackflag = append(brackflag,i));
+        flags = append(flags,brackflag));
+    return(flags))
+
+f = "allconflags.txt" << ""
+for flag in conflags(N) do(
+    f << flag << endl)
+g = "allbrackflags.txt" << ""
+for flag in brackflags(N) do(
+    g << flag << endl)
+
+quit()
+
+
